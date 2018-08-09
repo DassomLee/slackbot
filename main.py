27736a7ebae3efcad79222690ba9d0405c0a980c
DassomLee <mosadeel8811@gmail.com>
2018-08-09 11:24:06 +0900

    today's slackbot

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from rtmbot.core import Plugin
 
 import secret
 
+def answer(text):
+    if "지영" in text:
+        reply = "왱"
+    elif "주사위" == text:
+        reply = str(random.randint(1, 6))
+    else:
+        reply = None
+    # reply는 반드시 존재하게 됨. 불렀어, 주사위, None --> slack과는 무관해
+    return reply
+
 
 class HelloPlugin(Plugin):
     def process_message(self, data):
-        if "애란" in data["text"]:
-            self.outputs.append([data["channel"], "불렀어?"])
-        elif "주사위" == data["text"]:
-            die = str(random.randint(1, 6))
-            self.outputs.append([data["channel"], die])
-        else:
-            pass
+        reply = answer(data["text"]) # assignment... 오른쪽에 있는  expression을 왼쪽에 담아.
+        if reply is not None:
+            self.outputs.append([data["channel"], reply])
+
 
 
 config = {
